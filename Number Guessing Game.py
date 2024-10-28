5396d57ddaee3f2e7e59e9ffc88885c6a8f05365
Asdmir786 <asmir.alams.com@gmail.com>
2024-10-28 20:25:16 +0500

    First Task Completed

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -7,5 +7,24 @@ TheNumber = random.randint(0, 10)
 #  You can give hints like “too high” or “too low” until they guess correctly.
 
 def GuessingABloodyNumber():
+    global TheNumber
     print("A Number from 0 to 10 = You Guess, if guess != correct: os.remove(system32).")
+    print("Starting.")
+    print("Generating a Number....")
+    print("Beeep.")
+    print("Booop.")
+    print("You Number has been Generated, Now tell me what is it. ")
+    while True:
+        userGuess = int(input(": "))
+        if userGuess > TheNumber:
+            print("Too Bigh Number. ")
+        elif userGuess < TheNumber:
+            print("Too Smallh Number. ")
+        elif userGuess == TheNumber:
+            print(f"You Won, Your Guesss {userGuess} is equal to the original number {TheNumber}.")
+            break
+        else:
+            print("Tf you doing? I said a number")
 
+
+GuessingABloodyNumber()
