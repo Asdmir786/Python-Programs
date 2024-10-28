1148220bcffd778be933a26651c2ad156f4f3eeb
Asdmir786 <asmir.alams.com@gmail.com>
2024-10-28 15:37:24 +0500

    save

---
+++ Number Guessing Game.py
@@ -0,0 +1,5 @@
+# Number Guessing Game
+
+# Concepts: Input handling, conditionals, random module.
+# Description: Create a game where the program randomly selects a number within a range, and the player has to guess it.
+#  You can give hints like “too high” or “too low” until they guess correctly.
